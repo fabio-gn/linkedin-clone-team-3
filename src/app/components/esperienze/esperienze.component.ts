47af9ee5074f40e4786220b82712c652326ef521
Fabio Giannasi <fabio.giannasi@outlook.com>
2023-08-22 16:38:19 +0200

    creato component esperienze

---
+++ src/app/components/esperienze/esperienze.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-esperienze',
+  templateUrl: './esperienze.component.html',
+  styleUrls: ['./esperienze.component.scss']
+})
+export class EsperienzeComponent {
+
+}
