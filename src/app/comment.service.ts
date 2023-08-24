8dabd8fe583f03734b3a6d5856fbead55556b7c2
LorenzoAluigi <134380077+LorenzoAluigi@users.noreply.github.com>
2023-08-24 16:43:04 +0200

    comment service

--- src/app/comment.service.ts
+++ src/app/comment.service.ts
@@ -12,7 +12,7 @@ export class CommentService {
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMmU0YjFmMTc1YzAwMTRjNTU4ZjAiLCJpYXQiOjE2OTI2MTAxMjMsImV4cCI6MTY5MzgxOTcyM30.ETmqHK7g4xFJsEHHrxxFrIvhpsdo4QBKPAyca7RcGOE';
   constructor(private http: HttpClient) {}
 
-  getPosts(): Observable<IPost[]> {
+  getComment(id: string): Observable<IPost[]> {
     const headers = new HttpHeaders({
       Authorization: `Bearer ${this.authToken}`,
     });
