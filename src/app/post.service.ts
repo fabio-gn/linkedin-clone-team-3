aa10b66db757129b46498f45944ffe3301df5a49
LorenzoAluigi <134380077+LorenzoAluigi@users.noreply.github.com>
2023-08-24 14:33:29 +0200

    bug fix

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,4 +1,3 @@
-import { IPost } from 'src/app/interfaces/ipost';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
