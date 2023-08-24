308be16cb7e10e9bbfa9b05b158baffa2b6fcd05
LorenzoAluigi <134380077+LorenzoAluigi@users.noreply.github.com>
2023-08-24 11:35:13 +0200

    add interfaces

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,22 @@
+import { IPost } from 'src/app/interfaces/ipost';
 import { PostService } from './../../post.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   constructor(private Svc: PostService) {}
+
+  posts!: IPost;
+
+  ngOnInit() {
+    this.getPost();
+  }
+
+  getPost(): any {
+    this.Svc.getPosts().subscribe((data) => console.log(data));
+  }
 }
