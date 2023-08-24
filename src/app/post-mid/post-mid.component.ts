e922d36035061f01ee137ece65fdaa40551bbc07
alevdg <alessandravanessadg@gmail.com>
2023-08-24 19:05:25 +0200

    edit e delete error 404

--- src/app/post-mid/post-mid.component.ts
+++ src/app/post-mid/post-mid.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { IPost } from '../interfaces/ipost';
-import { ServiceService } from '../service.service';
-import { IProfile } from '../interfaces/profile';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-mid',
@@ -9,27 +8,31 @@ import { IProfile } from '../interfaces/profile';
   styleUrls: ['./post-mid.component.scss']
 })
 export class PostMidComponent {
-@Input() post!: IPost
+  @Input() post!: IPost;
+  newText: string = '';
+  posts: IPost[] = [];
 
+  constructor(private postService: PostService) { }
 
+  updatePost(postId: string, text: string) {
+    console.log('updatePost called with postId:', postId, 'and text:', text);
+    this.postService.updatePost(postId, { text }).subscribe((updatedPost) => {
+      // Update the UI to display the new text of the post
+      this.post.text = updatedPost.text;
+
+      // Display a message to the user
+      alert('Post updated successfully!');
+    });
+  }
+
+  deletePost(postId: string) {
+    console.log('deletePost called with postId:', postId);
+    this.postService.deletePost(postId).subscribe(() => {
+      // Remove the post from an array of posts
+      this.posts = this.posts.filter((post: IPost) => post._id !== postId);
+      // Display a message to the user
+      alert('Post deleted successfully!');
+    });
+  }
 
 }
-
-
-
-
-
-
-
-
-// constructor(private svc: ServiceService){
-
-// }
-
-// profilo!:IProfile
-// collegamenti!:IProfile[]
-// ngOnInit(){
-//   this.svc.getMe().subscribe(profilo => this.profilo = profilo)
-//   this.svc.getAll().subscribe(collegamenti => this.collegamenti = collegamenti)
-// }
-// }
