1bb46121d573542db8b55164a421294216736434
alevdg <alessandravanessadg@gmail.com>
2023-08-25 17:20:44 +0200

    delete comment test

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IComment } from '../interfaces/icomment';
 import { IPost } from '../interfaces/ipost';
+import { CommentService } from '../comment.service';
 
 @Component({
   selector: 'app-comment',
@@ -10,4 +11,17 @@ import { IPost } from '../interfaces/ipost';
 export class CommentComponent {
   @Input() comment!: IComment;
   @Input() post!: IPost;
+  comments: IComment[] = [];
+
+  constructor(private commentService: CommentService) { }
+
+  deleteComment(comment: string) {
+    console.log('deleteComment called with comment:', comment);
+    this.commentService.deleteComment(comment).subscribe(() => {
+      // Remove the comment from an array of comments
+      this.comments = this.comments.filter((c: IComment) => c.comment !== comment);
+      // Display a message to the user
+      alert('Comment deleted successfully!');
+    });
+  }
 }
